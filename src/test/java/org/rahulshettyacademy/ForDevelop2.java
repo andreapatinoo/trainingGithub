706a81a59e73c16ef40c15beb3c6cab94a6259f5
Andrea Patiño Orrego <andrea.patino@globant.com>
2026-06-15 23:58:08 -0500

    new develop2 branch

---
+++ src/test/java/org/rahulshettyacademy/ForDevelop2.java
@@ -0,0 +1,14 @@
+package org.rahulshettyacademy;
+
+import org.testng.annotations.Test;
+
+public class ForDevelop2 {
+
+    @Test
+    public void TestDevelop2(){
+        System.out.println("This is my own demo");
+        System.out.println("This is my own demoArch");
+        System.out.println("This is my own demoSameArch");
+        System.out.println("This is my own demoSameArch2");
+    }
+}
