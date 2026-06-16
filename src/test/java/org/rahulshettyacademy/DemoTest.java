d5596eb214a5f2db92c0606538d788827b0e61c2
Andrea Patiño Orrego <andrea.patino@globant.com>
2026-06-15 23:34:11 -0500

    First branch

--- src/test/java/org/rahulshettyacademy/DemoTest.java
+++ src/test/java/org/rahulshettyacademy/DemoTest.java
@@ -64,6 +64,17 @@ WebDriver driver;
     @Test
     public void Test(){
         System.out.println("This is my own demo");
+        System.out.println("This is my own demoArch");
+        System.out.println("This is my own demoSameArch");
+        System.out.println("This is my own demoSameArch2");
+    }
+
+    @Test
+    public void Test2Arch2(){
+        System.out.println("This is my own demo");
+        System.out.println("This is my own demoArch");
+        System.out.println("This is my own demoSameArch");
+        System.out.println("This is my own demoSameArch2");
     }
 
 
